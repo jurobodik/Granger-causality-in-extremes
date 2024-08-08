16f8897bd8a0438f30985fdcede8085931858774
Juraj Bodik <81770028+jurobodik@users.noreply.github.com>
2024-08-08 13:15:28 +0200

    Update Code.R

--- Simulations and applications/Crypto-Application/Code.R
+++ Simulations and applications/Crypto-Application/Code.R
@@ -5,7 +5,7 @@
 library("readxl")
 library(igraph)
 
-
+set.seed(0)
 data_origin = read.csv(file = 'data_last_day.csv')
 head(data_origin)
 
@@ -20,32 +20,23 @@ for (i in 1:14) {
 data = as.data.frame(data)
 names(data) = c('x1', 'x2', 'x3', 'x4', 'x5', 'x6', 'x7', 'x8', 'x9', 'x10', 'x11', 'x12', 'x13', 'x14')
 head(data)
+data = abs(data)
 
 
 #Just the simple plot 
 plot(data$x9, type = 'l', col=2, lty = 1, lwd = 2, xlab = 'Time [minutes]', ylab = 'Log-return values', main = 'IOTA and Binance')
 lines(data$x1, type = 'l', lty = 1, lwd = 2)
 
-
-
-lag_future=1; 
-data = abs(data) 
-
-Estimated_graph=Extreme_causality_full_graph_estimate(w=data, lag_future = lag_future, both_tails = TRUE)
-
-
+#Estimated graph using Algorithm 2 incorporating Algorithm 1
+Estimated_graph=Extreme_causality_full_graph_estimate(w=data, lag_future = 1, both_tails = TRUE)
 
 #We use igraph package to visualize it:
-dumping_factor = 0; #First figure
-dumping_factor = 0.2;  #Second figure
-
+dumping_factor = 0 #increase if interested only in the strongest edges
 G=list(G= Estimated_graph$G, weights = Estimated_graph$weights)
 G$G <- Estimated_graph$G[which(Estimated_graph$weights >= dumping_factor), ]; G$weights <- Estimated_graph$weights[which(Estimated_graph$weights >= dumping_factor)]
 graph <- graph_from_edgelist(G$G)
 E(graph)$edge.width <- 10*(G$weights)
 V(graph)$name =  c("Binance Coin", "Bitcoin", "BCH", "Cardano", "Dogecoin", "EOS.IO", "Ethereum", "Ethereum Classic", "IOTA", "Litecoin", "Maker", "Monero","Stellar", "TRON" )
-
-
 plot.igraph(graph, 
             layout = layout_with_fr(graph), 
             vertex.label = V(graph)$name, 
@@ -57,55 +48,43 @@ plot.igraph(graph,
 
 
 
-
-#If you want to obtain the estimates based on p-values, you can use the following function:
-#Estimated_graph=Extreme_causality_full_graph_estimate(w=data, lag_future = lag_future, both_tails = TRUE, p_value_based = TRUE)
-#However, this will take a few days to compute on a fast computer
-#In general, the size of Gamma coefficients corresponds to the p-values, so it is not necessary to compute all of them. 
-#You will obtain the same graph as using the previous method with dumping_factor = 0.2
-
-#Alternatively, you can just try computing only some specific edges that you are interested in
-#For example, is there a line from x1->x2 conditioned on other variables?
-x = data$x1; y = data$x2; z = data.frame(data$x3, data$x4, data$x5, data$x6, data$x7, data$x8, data$x9, data$x10, data$x11, data$x12, data$x13, data$x14)
-Extreme_causality_test(x = x, y = y, z = z, lag_future = 30,  p_value_computation = TRUE)
-
-
-
-
-
-
-#We do the same with lag = 30
-lag_future=30
-Estimated_graph=Extreme_causality_full_graph_estimate(w=data, lag_future = lag_future)
-
-
+#Estimated graph using Algorithm 2 incorporating the testing procedure, where an edge is present only if its corresponding p-value is below 0.05
+#Note that this takes a few hours to compute
+Estimated_graph=Extreme_causality_full_graph_estimate(w=data, lag_future = 1, both_tails = TRUE, p_value_based = TRUE, p_value_cutoff = 0.06)
 
 #We use igraph package to visualize it:
-dumping_factor = 0.3
 G=list(G= Estimated_graph$G, weights = Estimated_graph$weights)
 G$G <- Estimated_graph$G[which(Estimated_graph$weights >= dumping_factor), ]; G$weights <- Estimated_graph$weights[which(Estimated_graph$weights >= dumping_factor)]
 graph <- graph_from_edgelist(G$G)
 E(graph)$edge.width <- 10*(G$weights)
-V(graph)$name =  c("Binance Coin", "Bitcoin", "BCH", "Cardano", "Dogecoin", "EOS.IO", "Ethereum", "Ethereum Classic", "IOTA", "Litecoin", "Maker", "Monero","Stellar", "TRON" )
+V(graph)$name =  c("Binance Coin", "Bitcoin", "BCH", "Cardano","Dogecoin", 
+                   "EOS.IO", "Ethereum", "Ethereum Classic","IOTA", "Litecoin", 
+                   "Maker", "Monero","Stellar", "TRON" )
+plot.igraph(graph)
 
 
-plot.igraph(graph, 
-            layout = layout_with_fr(graph), 
-            vertex.label = V(graph)$name, 
-            edge.width = E(graph)$edge.width, 
-            vertex.color = 'lightgrey', 
-            vertex.label.cex = 1, 
-            vertex.size =22, 
-            main = 'Applying Algorithm 1, lag = 1 min')
-#Again, if you are willing to wait a few days, you can use 
-#Estimated_graph=Extreme_causality_full_graph_estimate(w=data, lag_future = lag_future, p_value_based = TRUE)
+#Finally, the same with lag = 30
+Estimated_graph=Extreme_causality_full_graph_estimate(w=data, lag_future = 30,both_tails = TRUE, 
+                                                      p_value_based = TRUE, p_value_cutoff = 0.05)
+
+G=list(G= Estimated_graph$G, weights = Estimated_graph$weights)
+G$G <- Estimated_graph$G[which(Estimated_graph$weights >= dumping_factor), ]; G$weights <- Estimated_graph$weights[which(Estimated_graph$weights >= dumping_factor)]
+graph <- graph_from_edgelist(G$G)
+E(graph)$edge.width <- 10*(G$weights)
+V(graph)$name =  c("Binance Coin", "Bitcoin", "BCH", "Cardano","Dogecoin", 
+                   "EOS.IO", "Ethereum", "Ethereum Classic","IOTA", "Litecoin", 
+                   "Maker", "Monero","Stellar", "TRON" )
+plot.igraph(graph)
+
+
+
 
 
 
 
 #Fun fact: We asked ChatGPT on May 2024 to draw causal graph between the variables. This is the result: 
 
-#Bitcoin --> Ethereum --> ERC-20 Tokens (e.g., Maker, IOTA, TRON, etc.)
+#Bitcoin --> Ethereum --> ERC-20 Tokens (e.g., Maker, IOTA)
 #|           |
 #             +--> Ethereum Classic
 #|
@@ -120,5 +99,6 @@ plot.igraph(graph,
 #  +--> BCH
 #|
 #  +--> EOS.IO
+
 
 
